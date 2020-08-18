68ac666fef4bf5e98414a241e581b89f7bda7ef6
Siddhesh-Ghadi <61187612+Siddhesh-Ghadi@users.noreply.github.com>
2020-08-18 12:43:22 +0530

    Create centos-conda-install.sh

---
+++ centos-conda-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+yum update -y
+yum install python3 -y
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-ppc64le.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+source "$HOME/miniconda/etc/profile.d/conda.sh"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+conda create -q -n test-environment python="3.6"
+conda activate test-environment
+conda install -y git
+conda config --prepend channels https://public.dhe.ibm.com/ibmdl/export/pub/software/server/ibm-ai/conda/
+export IBM_POWERAI_LICENSE_ACCEPT=yes
+conda install --yes --quiet conda-forge-ci-setup=2 conda-build=3.19.2 -c conda-forge
