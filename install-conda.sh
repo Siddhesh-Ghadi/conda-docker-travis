e61836863f06abb03716c9b45c226cdf19e775ea
Siddhesh-Ghadi <61187612+Siddhesh-Ghadi@users.noreply.github.com>
2020-08-18 12:07:27 +0530

    Update install-conda.sh

--- install-conda.sh
+++ install-conda.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update -y
-apt-get install build-essential -y
+apt-get install curl -y
+#apt-get install build-essential -y
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-ppc64le.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 source "$HOME/miniconda/etc/profile.d/conda.sh"
