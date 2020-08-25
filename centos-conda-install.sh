6d9fb4a0c283296077fb3da33d14e6fc7fb4a696
Siddhesh-Ghadi <61187612+Siddhesh-Ghadi@users.noreply.github.com>
2020-08-25 11:33:22 +0530

    Update centos-conda-install.sh

--- centos-conda-install.sh
+++ centos-conda-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yum update -y
 yum install python3 wget gcc g++ make curl -y
-ifconfig
+ip link
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-ppc64le.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 source "$HOME/miniconda/etc/profile.d/conda.sh"
